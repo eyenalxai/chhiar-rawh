abf6844da18b3a17ca1f598945abaeaafb47e5d2
Sax Dhiut <mail@iyxeyl.me>
2024-07-09 20:01:54 +0400

    feat: add drizzle url to server and runtime environment
    This update adds AUTH_DRIZZLE_URL to both the server and runtime environment configurations, allowing the system to leverage Drizzle authentication.

--- lib/env.ts
+++ lib/env.ts
@@ -5,11 +5,13 @@ export const env = createEnv({
 	server: {
 		AUTH_SECRET: z.string().min(16),
 		AUTH_REDDIT_ID: z.string().min(5),
-		AUTH_REDDIT_SECRET: z.string().min(5)
+		AUTH_REDDIT_SECRET: z.string().min(5),
+		AUTH_DRIZZLE_URL: z.string().min(5)
 	},
 	runtimeEnv: {
 		AUTH_SECRET: process.env.AUTH_SECRET,
 		AUTH_REDDIT_ID: process.env.AUTH_REDDIT_ID,
-		AUTH_REDDIT_SECRET: process.env.AUTH_REDDIT_SECRET
+		AUTH_REDDIT_SECRET: process.env.AUTH_REDDIT_SECRET,
+		AUTH_DRIZZLE_URL: process.env.AUTH_DRIZZLE_URL
 	}
 })

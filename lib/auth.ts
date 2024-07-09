d5cd18235399805dece2a6feec93e2b834f2376f
Sax Dhiut <mail@iyxeyl.me>
2024-07-09 20:02:02 +0400

    feat: enhance jwt callback in auth module
    The jwt callback in the auth module has been enhanced to not only log user and account information, but also to insert or update this data in the users and accounts database tables. This added functionality ensures that user and account data are consistently updated and maintained in our system.

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,3 +1,4 @@
+import { accounts, db, users } from "@/lib/schema"
 import NextAuth from "next-auth"
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
@@ -16,14 +17,47 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
 		}
 	],
 	callbacks: {
-		async jwt(stuff) {
-			console.log("token", stuff.token)
-			console.log("user", stuff.user)
-			console.log("account", stuff.account)
-			return stuff.token
+		async jwt({ user, account, token }) {
+			if (user && account) {
+				console.log("user", user)
+				console.log("account", account)
+				const [userModel] = await db
+					.insert(users)
+					.values({
+						username: user.name as string
+					})
+					.returning()
+					.onConflictDoUpdate({
+						target: users.username,
+						set: {
+							username: user.name as string
+						}
+					})
+
+				console.log("userModel", userModel)
+
+				await db
+					.insert(accounts)
+					.values({
+						userId: userModel.id,
+						refresh_token: account.refresh_token,
+						access_token: account.access_token,
+						expires_at: account.expires_at
+					})
+					.onConflictDoUpdate({
+						target: accounts.userId,
+						set: {
+							refresh_token: account.refresh_token,
+							access_token: account.access_token,
+							expires_at: account.expires_at
+						}
+					})
+			}
+			return token
 		},
 
 		async session({ session, token }) {
+			console.log("session", session)
 			return {
 				...session,
 				id: token.id

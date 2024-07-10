29c4acb0dbf954fffa3ff92c004aba113d8837c9
Sax Dhiut <mail@iyxeyl.me>
2024-07-10 12:27:42 +0400

    refactor: split post component into post and content components
    Split the previous single Post component into two separate components: Post and PostContent. This was done to allow better handling of crossposts. Now, the Post component checks for any cross post and the PostContent component manages the actual content rendering.

--- components/post.tsx
+++ components/post.tsx
@@ -5,39 +5,56 @@ import type { RedditPostData } from "@/types/reddit"
 import { ArrowDownIcon, ArrowUpIcon, MessageCircleIcon } from "lucide-react"
 import Markdown from "react-markdown"
 
+type PostContentProps = {
+	data: RedditPostData
+}
+
+export const PostContent = ({ data }: PostContentProps) => {
+	return (
+		<div className={cn("flex", "flex-col", "justify-center", "items-start", "gap-1")}>
+			<div className={cn("w-full", "flex", "flex-col", "items-start", "justify-between", "gap-2")}>
+				<h3>{data.title}</h3>
+				<PostImage data={data} />
+			</div>
+			<Markdown>{data.selftext}</Markdown>
+			<div className={cn("flex", "flex-row", "gap-1", "items-center")}>
+				<span className={cn("font-semibold", "text-sm")}>u/{data.author}</span>
+				<span className={cn("text-sm")}>at</span>
+				<span className={cn("text-sm")}>r/{data.subreddit}</span>
+			</div>
+			<div className={cn("flex", "flex-row", "items-center", "gap-2")}>
+				<div className={cn("flex", "flex-row", "items-center", "gap-0.5")}>
+					<MessageCircleIcon className={cn("size-4")} />
+					<span className={cn("text-sm")}>{data.num_comments}</span>
+				</div>
+				<div className={cn("flex", "flex-row", "items-center", "gap-0.5")}>
+					<ArrowUpIcon className={cn("size-4")} />
+					<span className={cn("text-sm")}>{data.ups}</span>
+				</div>
+				<div className={cn("flex", "flex-row", "items-center", "gap-0.5")}>
+					<ArrowDownIcon className={cn("size-4")} />
+					<span className={cn("text-sm")}>{Math.round(data.ups * (1 - data.upvote_ratio))}</span>
+				</div>
+			</div>
+		</div>
+	)
+}
+
 type PostProps = {
 	data: RedditPostData
 }
 
 export const Post = ({ data }: PostProps) => {
+	if (data.crosspost_parent_list && data.crosspost_parent_list.length > 0) {
+	}
+
 	return (
 		<Card className={cn("w-full", "max-w-md", "p-4")}>
-			<div className={cn("flex", "flex-col", "justify-center", "items-start", "gap-1")}>
-				<div className={cn("w-full", "flex", "flex-col", "items-start", "justify-between", "gap-2")}>
-					<h3>{data.title}</h3>
-					<PostImage data={data} />
-				</div>
-				<Markdown>{data.selftext}</Markdown>
-				<div className={cn("flex", "flex-row", "gap-1", "items-center")}>
-					<span className={cn("font-semibold", "text-sm")}>u/{data.author}</span>
-					<span className={cn("text-sm")}>at</span>
-					<span className={cn("text-sm")}>r/{data.subreddit}</span>
-				</div>
-				<div className={cn("flex", "flex-row", "items-center", "gap-2")}>
-					<div className={cn("flex", "flex-row", "items-center", "gap-0.5")}>
-						<MessageCircleIcon className={cn("size-4")} />
-						<span className={cn("text-sm")}>{data.num_comments}</span>
-					</div>
-					<div className={cn("flex", "flex-row", "items-center", "gap-0.5")}>
-						<ArrowUpIcon className={cn("size-4")} />
-						<span className={cn("text-sm")}>{data.ups}</span>
-					</div>
-					<div className={cn("flex", "flex-row", "items-center", "gap-0.5")}>
-						<ArrowDownIcon className={cn("size-4")} />
-						<span className={cn("text-sm")}>{Math.round(data.ups * (1 - data.upvote_ratio))}</span>
-					</div>
-				</div>
-			</div>
+			{data.crosspost_parent_list && data.crosspost_parent_list.length > 0 ? (
+				<PostContent data={data.crosspost_parent_list[0]} />
+			) : (
+				<PostContent data={data} />
+			)}
 		</Card>
 	)
 }

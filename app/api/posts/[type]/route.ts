633aa15c9c7e604d3b493d51c0012925d3cd67c2
Sax Dhiut <mail@iyxeyl.me>
2024-07-10 21:36:22 +0400

    feat: add pagination support to posts API
    This commit modifies the GET function in the posts API to add support for pagination. The 'after' value is now retrieved from the request's search parameters and, if present, added to the parameters sent to the getPostsServer function.

--- app/api/posts/[type]/route.ts
+++ app/api/posts/[type]/route.ts
@@ -3,7 +3,7 @@ import { getPostsServer } from "@/lib/fetch/server/posts"
 import type { PostsType } from "@/types/reddit"
 import { NextResponse } from "next/server"
 
-export const GET = async ({ params }: { params: { type: string } }) => {
+export const GET = async (request: Request, { params }: { params: { type: string } }) => {
 	const session = await auth()
 
 	if (!session) return new NextResponse("Unauthorized", { status: 401 })
@@ -13,7 +13,15 @@ export const GET = async ({ params }: { params: { type: string } }) => {
 	if (type !== "hot" && type !== "new" && type !== "top" && type !== "rising")
 		return new NextResponse("Not Found", { status: 404 })
 
-	const posts = await getPostsServer({ accessToken: session.accessToken, type: type })
+	const { searchParams } = new URL(request.url)
+
+	const after = searchParams.get("after")
+
+	const posts = await getPostsServer({
+		accessToken: session.accessToken,
+		type: type,
+		...(after ? { after: after } : {})
+	})
 
 	return NextResponse.json(posts)
 }

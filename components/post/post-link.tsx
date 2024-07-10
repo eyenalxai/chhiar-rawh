a533bb13d915884f07739631e1ecf921916fa403
Sax Dhiut <mail@iyxeyl.me>
2024-07-10 13:37:17 +0400

    feat: add error handling for fetch in url-metadata
    The update includes error handling for the fetch call in url-metadata. If the response is not ok, it now throws an error with status information, thereby improving its robustness against potential issues with URL metadata fetching.

--- components/post/post-link.tsx
+++ components/post/post-link.tsx
@@ -13,6 +13,7 @@ export const PostLink = ({ url }: PostLinkProps) => {
 		queryKey: ["url-metadata", { url }],
 		queryFn: async () =>
 			await fetch(`/api/url-metadata?url=${url}`).then(async (res) => {
+				if (!res.ok) throw new Error(`Failed to fetch metadata. Status: ${res.status}`)
 				const json = (await res.json()) as unknown as UrlMetadataResponse
 				if ("error" in json && json.error) throw new Error(json.error)
 				return json as unknown as typeof urlMetadatas.$inferSelect

276bc35c56e9b08ad50a8e96a9a42bae3731e0b4
Sax Dhiut <mail@iyxeyl.me>
2024-07-10 21:58:28 +0400

    feat: implement infinite scrolling for posts
    Added 'useInfiniteQuery' to fetch posts in pages and infinite scrolling ability. Replaced 'useQuery' with 'useInfiniteQuery' and added 'fetchNextPage' function for the 'Load More' button. This improves user experience by not loading all posts at once and providing a smoother browsing experience.

--- components/post/posts.tsx
+++ components/post/posts.tsx
@@ -1,28 +1,46 @@
 "use client"
 
 import { Post } from "@/components/post/post"
+import { Button } from "@/components/ui/button"
 import { getPostsClient } from "@/lib/fetch/client/posts"
 import { cn } from "@/lib/utils"
-import type { PostsType } from "@/types/reddit"
-import { useQuery } from "@tanstack/react-query"
+import type { PostsType, RedditListingObj, RedditPostObj } from "@/types/reddit"
+import { useInfiniteQuery, useQueryClient } from "@tanstack/react-query"
 
 type PostsProps = {
 	type: PostsType
 }
 
 export const Posts = ({ type }: PostsProps) => {
-	const { data: posts } = useQuery({
-		queryKey: [type],
-		queryFn: () => getPostsClient({ type })
+	const queryClient = useQueryClient()
+
+	const { data: postsPages, fetchNextPage } = useInfiniteQuery({
+		queryKey: [type, "infinite"],
+		queryFn: ({ pageParam }) => getPostsClient({ type, ...(pageParam ? { after: pageParam } : {}) }),
+		initialPageParam: undefined as string | undefined,
+		getNextPageParam: (lastPage) => lastPage.data.after,
+		initialData: () => {
+			const queryCacheData = queryClient.getQueryState([type])?.data as RedditListingObj<RedditPostObj> | undefined
+			if (queryCacheData) {
+				return {
+					pages: [queryCacheData],
+					pageParams: [undefined],
+					pageCount: 1
+				}
+			}
+		}
 	})
 
-	if (!posts) return null
+	console.log("postsPages", postsPages)
+
+	if (!postsPages) return null
 
 	return (
 		<div className={cn("flex", "flex-col", "gap-2", "w-full", "items-center")}>
-			{posts.data.children.map((post) => (
-				<Post key={post.data.title} data={post.data} />
-			))}
+			{postsPages.pages.map((page) =>
+				page.data.children.map((post) => <Post key={post.data.title} data={post.data} />)
+			)}
+			<Button onClick={() => fetchNextPage()}>Load More</Button>
 		</div>
 	)
 }

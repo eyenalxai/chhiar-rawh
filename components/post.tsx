e3e5234441703ea436174b03921b6c33fc370620
Sax Dhiut <mail@iyxeyl.me>
2024-07-10 10:57:27 +0400

    feat: update layout and style of post component
    This commit modifies the layout of the post component to be more clearly structured and visually appealing. The character sizes were adjusted for better readability and the layout was switched from row to column for a more logical and clean display of information.

--- components/post.tsx
+++ components/post.tsx
@@ -10,23 +10,26 @@ type PostProps = {
 export const Post = ({ data }: PostProps) => {
 	return (
 		<Card className={cn("w-full", "max-w-md", "p-2")}>
-			<div className={cn("flex", "flex-row", "gap-1")}>
-				<span className={cn("font-semibold")}>u/{data.author}</span>
-				<span>at</span>
-				<span>r/{data.subreddit}</span>
-			</div>
-			<div className={cn("flex", "flex-row", "items-center", "gap-2")}>
-				<div className={cn("flex", "flex-row", "items-center", "gap-0.5")}>
-					<MessageCircleIcon className={cn("size-4")} />
-					<span>{data.num_comments}</span>
+			<div className={cn("flex", "flex-col", "justify-center", "items-start", "gap-1")}>
+				<h3 className={cn("font-semibold", "text-lg", "leading-5")}>{data.title}</h3>
+				<div className={cn("flex", "flex-row", "gap-1", "items-center")}>
+					<span className={cn("font-semibold", "text-sm")}>u/{data.author}</span>
+					<span className={cn("text-sm")}>at</span>
+					<span className={cn("text-sm")}>r/{data.subreddit}</span>
 				</div>
-				<div className={cn("flex", "flex-row", "items-center", "gap-0.5")}>
-					<ArrowUpIcon className={cn("size-4", "text-amber-500")} />
-					<span>{data.ups}</span>
-				</div>
-				<div className={cn("flex", "flex-row", "items-center", "gap-0.5")}>
-					<ArrowDownIcon className={cn("size-4", "text-blue-500")} />
-					<span>{data.downs}</span>
+				<div className={cn("flex", "flex-row", "items-center", "gap-2")}>
+					<div className={cn("flex", "flex-row", "items-center", "gap-0.5")}>
+						<MessageCircleIcon className={cn("size-4")} />
+						<span className={cn("text-sm")}>{data.num_comments}</span>
+					</div>
+					<div className={cn("flex", "flex-row", "items-center", "gap-0.5")}>
+						<ArrowUpIcon className={cn("size-4", "text-amber-500")} />
+						<span className={cn("text-sm")}>{data.ups}</span>
+					</div>
+					<div className={cn("flex", "flex-row", "items-center", "gap-0.5")}>
+						<ArrowDownIcon className={cn("size-4", "text-blue-500")} />
+						<span className={cn("text-sm")}>{data.downs}</span>
+					</div>
 				</div>
 			</div>
 		</Card>

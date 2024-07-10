8c4d390859c4239ed33ada6e99b84b6017095286
Sax Dhiut <mail@iyxeyl.me>
2024-07-10 16:12:44 +0400

    feat: create image optimization route and refactor image loader
    Image optimization logic has been moved from `image-loader.ts` to a dedicated API route. API route validates `imageUrl` and `width` query parameters and generates an optimized image URL using `imgproxy`. `image-loader.ts` has been simplified to construct a URL for the new API route.

---
+++ app/api/image-optimization/route.ts
@@ -0,0 +1,36 @@
+import { env } from "@/lib/env"
+import { generateImageUrl } from "@imgproxy/imgproxy-node"
+import { NextResponse } from "next/server"
+
+export const GET = async (request: Request) => {
+	const { searchParams } = new URL(request.url)
+
+	const imageUrl = searchParams.get("imageUrl")
+	if (!imageUrl) return new NextResponse("imageUrl query param is required", { status: 400 })
+
+	const widthString = searchParams.get("width")
+	if (!widthString) return new NextResponse("width query param is required", { status: 400 })
+
+	const width = Number.parseInt(widthString)
+
+	if (Number.isNaN(width)) return new NextResponse("width query param must be a number", { status: 400 })
+
+	const optimizedImageUrl = generateImageUrl({
+		endpoint: "http://192.168.1.135:8080",
+		url: imageUrl,
+		options: {
+			resizing_type: "fill",
+			width: width,
+			gravity: {
+				type: "ce"
+			},
+			enlarge: 1,
+			format: "webp",
+			quality: 80
+		},
+		salt: env.IMGPROXY_SALT,
+		key: env.IMGPROXY_KEY
+	})
+
+	return NextResponse.redirect(optimizedImageUrl)
+}
--- lib/image-loader.ts
+++ lib/image-loader.ts
@@ -1,26 +1,8 @@
-import { env } from "@/lib/env"
-import { generateImageUrl } from "@imgproxy/imgproxy-node"
-
 type ImageLoaderProps = {
 	src: string
 	width: number
-	quality?: number
 }
 
-export default function imgProxyLoader({ src, width, quality }: ImageLoaderProps): string {
-	return generateImageUrl({
-		endpoint: "http://192.168.1.135:8080",
-		url: src,
-		options: {
-			resizing_type: "fill",
-			width: width,
-			gravity: {
-				type: "ce"
-			},
-			enlarge: 1,
-			format: "webp"
-		},
-		salt: env.IMGPROXY_SALT,
-		key: env.IMGPROXY_KEY
-	})
+export default function imgProxyLoader({ src, width }: ImageLoaderProps) {
+	return `/api/image-optimization?imageUrl=${encodeURIComponent(src)}&width=${width}` // This URL will be used by next/image to fetch the optimized image
 }

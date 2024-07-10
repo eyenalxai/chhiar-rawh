89587a88c9f98aa5dbd86de801d8d5780fc7bd50
Sax Dhiut <mail@iyxeyl.me>
2024-07-10 22:11:00 +0400

    feat: implement infinite scroll for posts
    Added 'react-intersection-observer' dependency and removed 'Load More' button to transition from pagination to infinite scrolling. The 'Post' component now takes an optional 'ref' prop, enabling it to be an intersection target. When the component is in view, the next page of posts is automatically fetched, providing a smoother user experience.

--- components/post/post.tsx
+++ components/post/post.tsx
@@ -5,14 +5,15 @@ import type { RedditPostData } from "@/types/reddit"
 
 type PostProps = {
 	data: RedditPostData
+	ref?: (node?: Element | null) => void
 }
 
-export const Post = ({ data }: PostProps) => {
+export const Post = ({ data, ref }: PostProps) => {
 	if (data.crosspost_parent_list && data.crosspost_parent_list.length > 0) {
 	}
 
 	return (
-		<Card className={cn("w-full", "max-w-md", "p-4")}>
+		<Card className={cn("w-full", "max-w-md", "p-4")} ref={ref}>
 			{data.crosspost_parent_list && data.crosspost_parent_list.length > 0 ? (
 				<PostContent data={data.crosspost_parent_list[0]} />
 			) : (
--- components/post/posts.tsx
+++ components/post/posts.tsx
@@ -1,17 +1,20 @@
 "use client"
 
 import { Post } from "@/components/post/post"
-import { Button } from "@/components/ui/button"
 import { getPostsClient } from "@/lib/fetch/client/posts"
 import { cn } from "@/lib/utils"
 import type { PostsType, RedditListingObj, RedditPostObj } from "@/types/reddit"
 import { useInfiniteQuery, useQueryClient } from "@tanstack/react-query"
+import { useEffect } from "react"
+import { useInView } from "react-intersection-observer"
 
 type PostsProps = {
 	type: PostsType
 }
 
 export const Posts = ({ type }: PostsProps) => {
+	const { ref, inView, entry } = useInView()
+
 	const queryClient = useQueryClient()
 
 	const { data: postsPages, fetchNextPage } = useInfiniteQuery({
@@ -31,14 +34,26 @@ export const Posts = ({ type }: PostsProps) => {
 		}
 	})
 
+	useEffect(() => {
+		if (inView) {
+			fetchNextPage().catch((error) => {
+				console.error("Error fetching next page:", error)
+			})
+		}
+	}, [inView, fetchNextPage])
+
 	if (!postsPages) return null
 
 	return (
 		<div className={cn("flex", "flex-col", "gap-2", "w-full", "items-center")}>
-			{postsPages.pages.map((page) =>
-				page.data.children.map((post) => <Post key={post.data.title} data={post.data} />)
+			{postsPages.pages.map((page, pageIdx) =>
+				page.data.children.map((post, postIdx) => {
+					if (pageIdx === postsPages.pages.length - 1 && postIdx === 2) {
+						return <Post key={post.data.title} data={post.data} ref={ref} />
+					}
+					return <Post key={post.data.title} data={post.data} />
+				})
 			)}
-			<Button onClick={() => fetchNextPage()}>Load More</Button>
 		</div>
 	)
 }
